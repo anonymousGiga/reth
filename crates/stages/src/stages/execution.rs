1f712471d4a24a54b5e9823fd44bddf87222e908
anonymousGiga <cryptonymGong@gmail.com>
2023-08-15 18:21:42 +0800

    test tps mgas

--- crates/stages/src/stages/execution.rs
+++ crates/stages/src/stages/execution.rs
@@ -24,6 +24,12 @@ use reth_provider::{
 use std::{ops::RangeInclusive, time::Instant};
 use tracing::*;
 
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    ops::Mul,
+};
+
 /// The execution stage executes all transactions and
 /// update history indexes.
 ///
@@ -67,6 +73,31 @@ pub struct ExecutionStage<EF: ExecutorFactory> {
     external_clean_threshold: u64,
     /// Pruning configuration.
     prune_modes: PruneModes,
+}
+
+#[inline(always)]
+fn rdtsc() -> u64 {
+    unsafe { core::arch::x86_64::_rdtsc() }
+}
+
+fn get_cpu_frequency() -> Option<f64> {
+    let file = File::open("/proc/cpuinfo").unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if let Some(freq_str) = line.strip_prefix("cpu MHz\t\t: ") {
+            let frequency: f64 = freq_str.parse().unwrap();
+            return Some(frequency * 1e6)
+        }
+    }
+
+    None
+}
+
+fn cycles_to_nanoseconds(cycles: u64, frequency: f64) -> u64 {
+    let ns_per_cycle = 1_000_000_000 as f64 / frequency;
+    (cycles as f64 * ns_per_cycle) as u64
 }
 
 impl<EF: ExecutorFactory> ExecutionStage<EF> {
@@ -131,6 +162,13 @@ impl<EF: ExecutorFactory> ExecutionStage<EF> {
         let mut state = PostState::default();
         state.add_prune_modes(prune_modes);
 
+        let fre = get_cpu_frequency().unwrap();
+        let mut txs_number: usize = 0;
+        let mut gas_used_in_m = 0.0f64;
+        let mut cnt = 0;
+        let mut start_time = rdtsc();
+        const N: u64 = 1000;
+
         for block_number in start_block..=max_block {
             let td = provider
                 .header_td_by_number(block_number)?
@@ -157,6 +195,28 @@ impl<EF: ExecutorFactory> ExecutionStage<EF> {
                     metrics_tx.send(MetricEvent::ExecutionStageGas { gas: block.header.gas_used });
             }
 
+            if cnt % N == 0 {
+                let end_time = rdtsc();
+                let cycles = end_time - start_time;
+                let elapsed_ns = cycles_to_nanoseconds(cycles, fre);
+                let tps = (txs_number * 1000_000_000) as u64 / elapsed_ns;
+                let mgas_ps = gas_used_in_m.mul(1000_000_000 as f64) / elapsed_ns as f64;
+                println!(
+                    "block_number = {:?}, tps = {:?}, mGas/s = {:?}",
+                    block_number, tps, mgas_ps
+                );
+
+                start_time = end_time;
+                txs_number = block.body.len();
+                gas_used_in_m =
+                    block.header.gas_used as f64 / reth_primitives::constants::MGAS_TO_GAS as f64;
+            } else {
+                txs_number += block.body.len();
+                gas_used_in_m +=
+                    block.header.gas_used as f64 / reth_primitives::constants::MGAS_TO_GAS as f64;
+            }
+            cnt += 1;
+
             // Merge state changes
             state.extend(block_state);
             stage_progress = block_number;
@@ -172,7 +232,12 @@ impl<EF: ExecutorFactory> ExecutionStage<EF> {
         // Write remaining changes
         trace!(target: "sync::stages::execution", accounts = state.accounts().len(), "Writing updated state to database");
         let start = Instant::now();
+        let start1 = rdtsc();
         state.write_to_db(provider.tx_ref(), max_block)?;
+        let end1 = rdtsc();
+        let cycles = end1 - start1;
+        let elapsed_ns = cycles_to_nanoseconds(cycles, fre);
+        println!("write_db: {:?}", elapsed_ns);
         trace!(target: "sync::stages::execution", took = ?start.elapsed(), "Wrote state");
 
         let done = stage_progress == max_block;
